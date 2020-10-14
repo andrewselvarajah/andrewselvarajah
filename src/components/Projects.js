64b4936ec15bf4c0be18e5adca3efc2730822563
Andrew Selvarajah <andrew.sevarajah@uoit.net>
2020-10-14 16:23:15 -0400

    aligned project items

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,13 +5,19 @@ function Projects(props){
   return(
      <div className = 'Projects'>
        <div className = "text-project">
+         <div style= {{top: "0px"}}>
          
-       <h1 className = "col-title">{props.title}</h1>
-       <p className = "sub">{props.subject}</p>
-       <form action={props.link } target = "_blank">
-         <button type="submit" className = "pro-github" target="_blank"><i className="fab fa-github-square fa-3x" /></button>
-      </form>
+          <h1 className = "col-title">{props.title}</h1>
+          <p className = "sub">{props.subject}</p>
        </div>
+       
+
+
+
+       </div>
+       <form action={props.link } target = "_blank" className = "form">
+          <button type="submit" className = "pro-github" target="_blank"><i className="fab fa-github-square fa-3x" /></button>
+        </form>
 
      </div>
 
